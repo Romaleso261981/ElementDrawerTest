144fd0c99c071068ff177d463fa2b1411f67987a
Romaleso261981 <ladiginscormag@gmail.com>
2025-10-05 12:53:19 +0300

    Update allowedHosts in vite.config.js to replace old host with new one.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      '978gqk-5173.csb.app',
+      'gw4xsj-5173.csb.app',
       'localhost',
       '127.0.0.1'
     ]
